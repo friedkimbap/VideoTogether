0cb3f1dd430bf3584fe4e4a51738bfddd01ff909
FriedKimBap <rladudals4356@gmail.com>
2024-11-22 14:39:19 +0900

    3

--- out/production/videoClient/index.js
+++ out/production/videoClient/index.js
@@ -1,22 +1,9 @@
 var player;
-var socket = new WebSocket('ws://localhost:54321');
-
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var socket = new WebSocket('ws://localhost:8887');
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         videoId: 'TfatKL3cI7I', // 유튜브 링크 뒤에 id 있음
-        playerVars: { // https://developers.google.com/youtube/player_parameters?hl=ko 참조
-            'controls': 0, // 플레이어 컨트롤 숨기기
-            'fs': 0, // 전체화면 버튼 숨기기
-            'modestbranding': 1, // YouTube 로고 숨기기
-            'rel': 0, // 관련 동영상 숨기기
-            'autoplay': 1, // 자동 재생
-            'enablejsapi': 1,
-        },
         events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
@@ -30,11 +17,11 @@ function onPlayerReady(event) {
 
 function onPlayerStateChange(event) {
     var action;
-    if (event.data === YT.PlayerState.PLAYING) {
+    if (event.data == YT.PlayerState.PLAYING) {
         action = 'play';
-    } else if (event.data === YT.PlayerState.PAUSED) {
+    } else if (event.data == YT.PlayerState.PAUSED) {
         action = 'pause';
-    } else if (event.data === YT.PlayerState.ENDED) {
+    } else if (event.data == YT.PlayerState.ENDED) {
         action = 'stop';
     }
     if (action) {
@@ -51,6 +38,12 @@ function playVideo() {
 function pauseVideo() {
     if (player) {
         player.pauseVideo();
+    }
+}
+
+function stopVideo() {
+    if (player) {
+        player.stopVideo();
     }
 }
 
